7d2e724f0d19d24e79d794974197fedb97deec13
Nurbolot <nubolotkadyrov06@gmail.com>
2025-04-30 00:32:38 +0600

    change type vacancy

--- src/shared/types/vacancy.types.ts
+++ src/shared/types/vacancy.types.ts
@@ -10,12 +10,15 @@ export interface IVacancy extends BaseI {
     remote: boolean;
     office: boolean;
     hybrid: boolean;
-
     timeWork: string;
-
     isArchived: boolean;
     userId: string;
     salary: string;
+
+    salaryFrom: string;
+    salaryTo: string;
+    minExperience: number;
+    maxExperience: number;
 
     user: Pick<IUser, 'username' | 'avatarUrl' | 'lastName' | 'name' | 'identification'>;
 }
