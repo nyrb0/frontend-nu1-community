e664285e5529fa6e49d2dcc62f385c55aee838f6
Nurbolot <nubolotkadyrov06@gmail.com>
2025-05-01 22:33:28 +0600

    auth promlem fixed

--- src/pages/auth/hooks/useRegis.ts
+++ src/pages/auth/hooks/useRegis.ts
@@ -3,14 +3,11 @@ import { IRegisAuth } from '@/shared/types/auth.types';
 import { ROLES } from '@/shared/types/roles';
 import { useState } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
-import { useNavigate } from 'react-router-dom';
 
 interface IFormData extends IRegisAuth {
     repeatPassword: string;
 }
-
 export const useRegis = () => {
-    const navigate = useNavigate();
     const [roles, setRoles] = useState<{ role: string; positionRole: string | null; speciality: string | null }>({
         role: ROLES.USER,
         positionRole: null,
@@ -25,31 +22,26 @@ export const useRegis = () => {
         watch,
     } = useForm<IFormData>();
     const [isVisibilitySpecification, setIsVisibilitySpecification] = useState(false);
-
     const password = watch('password');
-
     const handlerRegister: SubmitHandler<IFormData> = async data => {
         try {
             clearErrors();
             // проверяем полю positionRole, speciality
-            console.log('jvnjfdvnhdvbdh');
             if ((!roles.positionRole || !roles.speciality) && roles.role === ROLES.USER) {
                 setError('speciality', { type: 'manual', message: 'Выберите специальность' });
                 setError('positionRole', { type: 'manual', message: 'Выберите позицию' });
                 return;
             }
-            const { repeatPassword, ...body } = { ...data, speciality: roles.speciality, positionRole: roles.positionRole };
-
+            const { repeatPassword, ...body } = { ...data, ...roles };
             const response = await authService.auth('regis', body);
             if (response.status === 200) {
                 localStorage.setItem('username', data.username);
-                navigate('/');
+                window.location.href = '/';
             }
         } catch (err) {
             console.log('Ошибка при регистрации', err);
         }
     };
-
     return {
         roles,
         setRoles,
